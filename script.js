4285b5f3cf29208d3c3d5facd91390db507b9f1e
Pritam Das <dpritam28577@gmail.com>
2022-10-04 20:13:39 +0530

    add the tabbed

--- script.js
+++ script.js
@@ -68,14 +68,37 @@ btnScrollTo.addEventListener('click', function (e) {
 //    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 //   });
 // });
-// 1.add evernt listener to common parent element 
-document.querySelector('.nav__links').addEventListener('click',function(e){
+// 1.add evernt listener to common parent element
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
-
-  if(e.target.classList.contains('nav__link')){
+  if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
-     console.log(id);
-     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+//tabbed componenet
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContaines = document.querySelector('.operations__tab-container');
+const tabsContents =document.querySelectorAll('.operations__content');
+
+tabsContaines.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContents.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
